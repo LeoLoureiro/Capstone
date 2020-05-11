eec949ae4ed7f9a1a9a3b192ae634648a26aad22
LeoLoureiro <49467947+LeoLoureiro@users.noreply.github.com>
2020-05-10 22:27:37 -0700

    Update PlayerModel.java

--- PlayerModel.java
+++ PlayerModel.java
@@ -1,13 +1,27 @@
+/*
+ * CSCV335 
+ * Final Capstone
+ * 
+ * Erik Ibarra Hurtado
+ * Samuel Bryant
+ * Leonardo Loureiro
+ * 
+ * Sunday, May 10, 2020
+ * 
+ * PlayerModel.java
+ * 
+ * PlayerModel contains each individual player's data as well as the dice that the player rolled.
+ * It also contains the points that the player has accumulated so far.
+ * Each player has a name, a playerId, points, and dice rolled.
+ * 
+ */
 package application;
-
 
 public class PlayerModel {
 	private String name;
 	private int playerId;
 	private int points;
 	private int[] diceRolled;
-	
-	
 	
 	public PlayerModel(String name, int playerId, int points, int[] diceRolled) {
 		setName(name);
