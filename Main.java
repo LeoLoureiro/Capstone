995c0feb0a303d5e6042070bfcbd7a1528465d08
LeoLoureiro <49467947+LeoLoureiro@users.noreply.github.com>
2020-05-10 19:49:53 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,14 @@
+pack/*
+ * Erik Ibarra Hurtado
+ * Samuel Bryant
+ * Leonardo Loureiro
+ * 
+ * Sunday, May 10, 2020
+ * 
+ * Main.java
+ * 
+ * 
+ * */
 package application;
 	
 import javafx.application.Application;
@@ -5,13 +16,13 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.fxml.FXMLLoader;
-
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("CapstoneDesign2View.fxml"));
+			GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("RulesView.fxml"));
 			Scene scene = new Scene(root);
+			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -21,7 +32,13 @@ public class Main extends Application {
 	}
 	
 	public static void main(String[] args) {
+		System.out.print("Design 3");
 		launch(args);
+	}
+	@Override
+	public void stop() {
+		System.out.println("stop has been called");
+		System.exit(0);
 	}
 	
 }
